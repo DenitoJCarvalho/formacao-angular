4eaa3613654ac8f6d0938a0ebb12db626afcafa1
Dennis J Carvalho <hdtecitu@gmail.com>
2021-12-10 20:14:25 -0300

    criado novos modulos, componentes, model e servicos

--- gerenciador-de-tarefas/src/app/tarefas/index.ts
+++ gerenciador-de-tarefas/src/app/tarefas/index.ts
@@ -1,4 +1,8 @@
 //exportando o module tarefax
 export * from './tarefas.module';
 //exportando o model shared  
-export * from './shared'; 
+export * from './shared';
+//exportando o component lista de tarefas
+export * from './listar';
+//exportando o modulo de roteamento
+export * from './tarefas-routing.module'
---
+++ gerenciador-de-tarefas/src/app/tarefas/shared/tarefa.model.ts
@@ -0,0 +1,12 @@
+/*
+  ? em typescript informa que o parâmetro é opcional
+*/
+
+
+export class Tarefa {
+  constructor(
+    public id?: number,
+    public nome?: string,
+    public concluida?: boolean
+  ) { }
+}
